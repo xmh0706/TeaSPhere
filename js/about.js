aa1227e14d272161c907ae957c2cc8584a96bfb7
xmh0706 <1148538989@qq.com>
2018-06-29 17:28:24 +0800

    6/29 modify

--- js/about.js
+++ js/about.js
@@ -1,16 +1,9 @@
 window.onload=function(){
-	
-	// autoPlayHW('autoPlay')
-//
 	$("#mainLeftLi li").click(function(){
 		flash = $(this).index()
 		$('#mainRight li').eq(flash).fadeIn(1000).siblings().hide()
 		$('#mainLeftLi li').eq(flash).addClass('leftListNow').siblings().removeClass('leftListNow')
 
 	})
-	// window.onscroll = function(){
-	
-	// }
 
-//
 }
--- js/public.js
+++ js/public.js
@@ -7,14 +7,14 @@ $(document).ready(function(){
 		$('.autoPlayWarp li').eq(flash).fadeIn(1000).siblings().hide()
 		$('.autoPlayDot li').eq(flash).addClass('choose').siblings().removeClass('choose')
 	}
-	var lunbo=setInterval(function(){autoRun()},3000)
+	var lunbo=setInterval(function(){autoRun()},4000)
 	$('.autoPlayWarp li').hover(function(){
 		clearInterval(lunbo)
 		flash = $(this).index()
 		$('.autoPlayWarp li').eq(flash).fadeIn(1000).siblings().hide()
 		$('.autoPlayDot li').eq(flash).addClass('choose').siblings().removeClass('choose')
 	},function(){
-		lunbo=setInterval(function(){autoRun()},3000)
+		lunbo=setInterval(function(){autoRun()},4000)
 	})
 	
 	$('.autoPlayDot li').hover(function(){
@@ -23,7 +23,7 @@ $(document).ready(function(){
 		$('.autoPlayWarp li').eq(flash).fadeIn(1000).siblings().hide()
 		$('.autoPlayDot li').eq(flash).addClass('choose').siblings().removeClass('choose')
 	},function(){
-		lunbo=setInterval(function(){autoRun()},3000)
+		lunbo=setInterval(function(){autoRun()},4000)
 	})
 ////获取轮播图片宽高
 	var image= new Image();
@@ -32,36 +32,18 @@ $(document).ready(function(){
 		var bigWd=image.width
 		var bigHt=image.height
 		var scale=bigHt/bigWd
-		// function autoPlayHW(id){
-		// 	var obj = document.getElementById(id);
-		// 	var wh=window.innerWidth
-		// 	var autoHt = wh*scale //autoPlayWarp高度	
-		// 	obj.style.height=autoHt+"px";
-		// 	obj.style.width=wh+"px";
-		// }
-		// autoPlayHW('autoPlayWarp')
 		var find = document.getElementById("autoPlayWarp").offsetWidth
 		$("#autoPlayWarp").css("height",find*scale +"px")
 	}	
 })
+
 $(window).resize(function(){
 	//获取轮播图片宽高
 	var image= new Image();
-	image.src="image/public/1C18E3E0-A482-49BB-8662-F3A62BA0A503@2x.png"	
-	image.onload=function(){
+	image.src="image/public/5DBBD51A-A150-49B1-83E0-D1F4A7673ED3@1x.png"	
 		var bigWd=image.width
 		var bigHt=image.height
 		var scale=bigHt/bigWd
-		// function autoPlayHW(id){
-		// 	var obj = document.getElementById(id);
-		// 	var wh=window.innerWidth
-		// 	var autoHt = wh*scale //autoPlayWarp高度	
-		// 	obj.style.height=autoHt+"px";
-		// 	obj.style.width=wh+"px";
-		// }
-		// autoPlayHW('autoPlayWarp')
 		var find = document.getElementById("autoPlayWarp").offsetWidth
-		$("#autoPlayWarp").css("height",find*scale +"px")
-	}	
-	// autoPlayHW('autoPlay')
+		$("#autoPlayWarp").css("height",find*scale +"px")	
 })
